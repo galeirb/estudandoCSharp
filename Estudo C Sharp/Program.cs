62f1274acb5fd87a0c9572441be251d36ad18d68
Gabriel Marques da Silva Abreu <30577462+galeirb@users.noreply.github.com>
2022-02-05 22:02:30 -0500

    Criação do projeto

---
+++ Estudo C Sharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Hello World! \n");  // Comando para escrever na tela
+            Console.Write("Meu nome é Gabriel Marques \n");
+            Console.ReadLine();             // Aguardar o usuário digitar
+        }
+    }
+}
